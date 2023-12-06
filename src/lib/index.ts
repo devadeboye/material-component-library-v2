3f79439f87fe2990dd19009d8518eeb600c1c1f3
Adeboye Emmanuel <emmanueladeboye2017@gmail.com>
2023-12-06 19:24:53 +0100

    add styles to components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,5 +5,7 @@ import {
 } from "./Button/SegmentedButton";
 import { SegmentedButtonState } from "./Button/interfaces/button.interface";
 
+import "../main.css";
+
 export { SegmentedButton, SegmentedButtonSizeEnum, SegmentedButtonStyleEnum };
 export type { SegmentedButtonState };
--- webpack.config.js
+++ webpack.config.js
@@ -18,11 +18,10 @@ module.exports = {
 				exclude: /node_modules/,
 				use: "babel-loader",
 			},
-			// {
-			// 	test: /\.tsx?$/,
-			// 	use: "ts-loader",
-			// 	exclude: /node_modules/,
-			// },
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
 		],
 	},
 	resolve: {
