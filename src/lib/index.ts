d787d24b3a47a856f2555ceeab4c1465b405e515
Adeboye Emmanuel <emmanueladeboye2017@gmail.com>
2023-12-07 09:45:13 +0100

    fix: export button enums

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,13 +5,19 @@ import {
 } from "./Button/SegmentedButton";
 import { SegmentedButtonState } from "./Button/interfaces/button.interface";
 import Button from "./Button/Button";
+import { ButtonStyleEnum, ButtonBorderEnum } from "./Button/Button";
 
 import "../main.css";
 
 export {
+	//segmented button
 	SegmentedButton,
 	SegmentedButtonSizeEnum,
 	SegmentedButtonStyleEnum,
+
+	// button
 	Button,
+	ButtonStyleEnum,
+	ButtonBorderEnum,
 };
 export type { SegmentedButtonState };
